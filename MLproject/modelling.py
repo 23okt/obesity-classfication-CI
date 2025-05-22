2d91f27a44c25e5f030f8dfcf15833dc9477dfc7
Muhammad Gilang Ramadhan <foryouandmefck17@gmail.com>
2025-05-22 11:38:30 +0700

    update modelling

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     
     n_estimators = int(sys.argv[1]) if len(sys.argv) > 1 else 100
     max_depth = int(sys.argv[2]) if len(sys.argv) > 2 else 10
-    train_file = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(__file__), '..', 'dataset', 'obesity_data_train_preprocessing.csv')
-    test_file = sys.argv[4] if len(sys.argv) > 4 else os.path.join(os.path.dirname(__file__), '..', 'dataset', 'obesity_data_test_preprocessing.csv')
-    
+    train_file = sys.argv[3] if len(sys.argv) > 3 else "obesity_data_train_preprocessing.csv"
+    test_file = sys.argv[4] if len(sys.argv) > 4 else "obesity_data_test_preprocessing.csv"
+
     
     train_df = pd.read_csv(train_file)
     test_df = pd.read_csv(test_file)
